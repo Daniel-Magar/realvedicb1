950085afc38e83456ce154eedf2e7a0166f3631e
DanielMagar <magar.t.daniel@gmail.com>
2022-08-25 23:37:12 +0530

    search

--- src/components/Rectangle.jsx
+++ src/components/Rectangle.jsx
@@ -5,13 +5,9 @@ const Rectangle = () => {
   return (
     <section>
       <div className="w-[90%] border border-purple-500 ml-5 mr-5 mt-2 p-3">
-        <div className="md:hidden w-full m-auto  p-2">
-          <input type="text" className="w-full p-3 border border-red-500" />
-          <img
-            src={search}
-            alt="search"
-            className="absolute p-10 min-w-[14px] top-[7rem] left-[13rem]"
-          />
+        <div className="md:hidden flex items-center justify-between border w-full m-auto p-1">
+          <input type="text" className="w-full p-2 outline-none border-none" />
+          <img src={search} alt="search" className="min-w-[14px]" />
         </div>
       </div>
     </section>
